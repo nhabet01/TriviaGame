f9cd945c98fdf1d5ce1239e976dd82148874a25a
Nahir Habet <Nahir Habet>
2017-01-11 17:47:45 -0500

    some minor changes to JS and html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,8 @@
-var questionCount = 0;    //Nahir JS
+//Trivia Game JS_NH
+
+document.addEventListener("DOMContentLoaded", function(){
+//setting up global variables
+var questionCount = 0;    
 var correctAnswer;
 var answerChoices;
 var trivData;
@@ -11,69 +15,61 @@ var missed = 0;
 var rightDiv = document.getElementById("right");
 var wrongDiv = document.getElementById("wrong");
 var missedDiv = document.getElementById("missed");
-var pauseButton = document.getElementById("pause");
-var resumeButton = document.getElementById("resume");
-var giphy;
+var startButton=document.getElementById("startButton");
 var qsArray = [];
 
 
 
-//Get the next Question from list: each is an Object!
-function nextQuestion(){
+  //Get the next Question from list: each is an Object!
+  function nextQuestion(){
 
-  console.log("Next Question/object:");
-    $('#main').empty(); //empty main div
+      console.log("Next Question/object iniated for Q:" + questionCount);
+      $('#main').empty(); //empty main div
 
-    var cat= $('<div id = "category">');
-  
-    $('#main').append(cat);
-
-    var q= $('<div id = "question" >');
-
-    $('#main').append(q);
-
-  timer = 10;
-
-  if(questionCount > 4){ //change this to final # when done testing
-    return gameScore();
-  }
-  //Set up Q's array
-  var qsArray = trivData[questionCount].incorrect_answers;
-  console.log("These are the answer options (qsArray= .incorrect_answers): " + qsArray);
-  correctAnswer = trivData[questionCount].correct_answer;
-  qsArray.push(trivData[questionCount].correct_answer);
-  console.log("updated qsArray: " + qsArray);
-
-  // qsArray = shuffle(qsArray);
-  //ansOptions will be passed into onclick event
-  ansOptions = document.getElementsByClassName("ansOptions");
-  timerDiv.innerHTML = "<p>Timer: " + timer + "</p>";
-  $('#category').html("Category: "+ trivData[questionCount].category);
-  $('#question').html(trivData[questionCount].question);
+      var cat= $('<div id = "category">');
     
-    var aZero= $('<div id = ans0 class=ansOptions>');
-    $('#main').append(aZero);
-    var aOne= $('<div id = ans1 class=ansOptions>');
-    $('#main').append(aOne);
-    var aTwo= $('<div id = ans2 class=ansOptions>');
-    $('#main').append(aTwo);
-    var aThree= $('<div id = ans3 class=ansOptions>');
-    $('#main').append(aThree);
+      $('#main').append(cat);
 
-      //ansOptions will be passed into onclick event
-    ansOptions = document.getElementsByClassName("ansOptions");
+      var q= $('<div id = "question" >');
 
+      $('#main').append(q);
 
-//Assigning each answer to its <div>:
-  for(var i = 0; i < qsArray.length; i++) {
+      timer = 10;
+
+    if(questionCount > 9){ //change this to final # when done testing
+      return gameScore();
+    }
+
+    //Set up Q's array
+    var qsArray = trivData[questionCount].incorrect_answers;
+    console.log("These are the answer options (qsArray= .incorrect_answers): " + qsArray);
+    correctAnswer = trivData[questionCount].correct_answer;
+    qsArray.push(trivData[questionCount].correct_answer);
+    console.log("updated qsArray: " + qsArray);
+
+    // qsArray = shuffle(qsArray);
+
+      timerDiv.innerHTML = "Time Remainig: " + timer + " Seconds";
+      $('#category').html("Category: "+ trivData[questionCount].category);
+      $('#question').html(trivData[questionCount].question);
     
-    // $('#main').append('<div class = "ansOptions" id=" ans'+[i]+'" />');
+        //ansOptions will be passed into onclick event
+      ansOptions = document.getElementsByClassName("ansOptions");
 
-    document.getElementById('ans'+ i).innerHTML = qsArray[i];
+
+    //creating ans divs and assigning each answer to its <div>:
+    for(var i = 0; i < qsArray.length; i++) {
+      
+      $('#main').append('<div class = ansOptions id= ans' +[i]+' >');
+
+      document.getElementById('ans'+ i).innerHTML = qsArray[i];
+    }
+      
+      //call fxn to check answer
+      answerOnClick(ansOptions);
+      //then call start fxn which starts the countdown fxn
+      start();
   }
-  answerOnClick(ansOptions);
-  start();
-}
   //Checking event/answer
   function answerOnClick(answers){
     Array.prototype.forEach.call(answers, function(element){
@@ -84,60 +80,46 @@ function nextQuestion(){
                     timer = 10;
           questionCount++;
           right++;
-          rightDiv.innerHTML = "<p>Correct: " + right;
-          // setTimeout(function(){nextQuestion();}, 5000);
         }
         else {
           stop();
-          alertMessage("Wrong!");
+          alertMessage("Nope!");
           timer = 10;
           questionCount++;
           wrong++;
-          wrongDiv.innerHTML = "<p>Wrong: " + wrong;
-          // setTimeout(function(){nextQuestion();}, 5000);
+
         }
       }
     })
   }
 
-  function disableAnswerOnClick(answers){
-    Array.prototype.forEach.call(answers, function(element){
-      element.onclick = function(){
-        alertMessagePaused("Paused!");
-      }
-    })
-  }
 
   function start(){
-    intervalID = setInterval(countDown, 1200);
+    intervalID = setInterval(countDown, 1000);
   }
 
   function countDown(){
     timer--;
-    timerDiv.innerHTML = "<p>Timer: " + timer + "</p>";
+    timerDiv.innerHTML = "Time Remainig: " + timer + " Seconds";
     if (timer === 0){
       stop();
       alertMessage("You're out of time");
       timer = 10;
       questionCount++;
       missed++;
-      missedDiv.innerHTML = "<p>missed: " + missed + "</p>";
+
     }
   }
 
   function stop(){
     clearInterval(intervalID);
   }
-
+  //calling Trivia queston from open trivia database.
   function callTrivia(){
+    var queryUrl = `https://opentdb.com/api.php?amount=10&category=22&type=multiple`;
     $.ajax({
-      url:'https://opentdb.com/api.php?amount=5',
-      // method: 'GET'
-      data: {
-       category: 22,
-        // difficulty: medium,
-        type: 'multiple'
-      }
+      url: queryUrl,
+      method: 'GET'
     }).done(function(response){
       trivData = response.results;
       console.log(trivData);
@@ -145,7 +127,7 @@ function nextQuestion(){
       nextQuestion();
     });
   }
-
+  //fxn to call giphy with tag related to the correct answer
   function callGiphy(search){
     search = search.replace(' ','+');
     $.ajax({
@@ -156,111 +138,110 @@ function nextQuestion(){
       }
 
     }).done(function(response) {
-      console.log(response);
-      giphy = response.data[0].images.fixed_height.url;
+      console.log("giphy data: " +response);
+      giphy = response.data[1].images.fixed_height.url;
       document.getElementById("giphy").innerHTML = "<img src=" + giphy + ">";
     });
   }
 
-  function gameScore(){
-    timer = 10;
-    timerDiv.innerHTML = "<p>Timer: " + timer + "</p>";
-    pauseButton.disabled = true;
-    resumeButton.disabled = true;
-    GameOver("All done, here's how you did:");
 
-  }
 
   function alertMessage(message){
+    //empty main div
+    $('#main').empty(); 
 
-    $('#main').empty(); //empty main div
-
-    var a= $('<p id = "response">').text(message + "The correct answer was: " + correctAnswer);
-   
-    
-    $('#main').append(a);
+    var a= $('<p id = response>').text(message +" The correct answer was: " + correctAnswer);
+    //adding a break to have "message" on a different line from rest of text (like instructions)
+    a.html(function(i,v){ return v.replace('The','<br>The');});
+        $('#main').append(a);
 
     var b= $('<div id = giphy >');
-
     $('#main').append(b);
 
-
     callGiphy(correctAnswer);
+    console.log("callGiphy fxn from alertMessage fxn 1st")
 
-    setTimeout(function(){nextQuestion();}, 4000);
+    setTimeout(nextQuestion, 4000);
+    console.log("nextQuestion called from alertMessage fxn 2nd");
+  }
 
+  function gameScore(){
+    console.log("gameScore called (q[i] surpassed limit)")
+    timer = 10;
+    timerDiv.innerHTML = "Time Remainig: " + timer + " Seconds";
 
+    // pauseButton.disabled = true;
+    // resumeButton.disabled = true;
+    GameOver("All done, here's how you did:");
+    questionCount=0;//This causes next quesion to try and use the first question object again.
   }
 
   function GameOver(message){
 
-    $('#main').empty(); //empty main div
+    stop();
 
+    $('#main').empty(); //empty main div
+    //create a variable to hold the element that displays the message
     var a= $('<p id = "gameResult">').text(message);
-    // a.attr('data-name', message);
-    
+    //append to the main div   
     $('#main').append(a);
 
     var corrects= $('<div id = correct >').text("Correct Answers: "+ right);
-
     $('#main').append(corrects);
 
-    var wrongs= $('<div id = wrong >').text("Wrong Answers: "+ wrong);
-
+    var wrongs= $('<div id = wrong >').text("Incorect Answers: "+ wrong);
     $('#main').append(wrongs);
 
-    var missed= $('<div id = missed >').text("Missed Questions: "+ missed);
+    var missedQs= $('<div id = missed >').text("Unanswered: "+ missed);
+    $('#main').append(missedQs);
 
-    $('#main').append(missed);
+    var b= $('<div id = giphy >');
+    $('#main').append(b);
 
+    startButton=$('<button type = button class= btn-lg id = startButton>').text("Start Over ?");
+    $('#main').append(startButton);
 
+    if (right/questionCount > 0.75){
+        callGiphy("Hooray");
+        console.log("callGiphy fxn called from GameOver fxn");
 
-    // callGiphy(" ");
+    }
+    else {
+        callGiphy("game over");
+        console.log("callGiphy fxn called from GameOver fxn");      
+    }
 
-    setTimeout(function(){nextQuestion();}, 4000);
-
-   
-
-  }
-
-
-
-  document.addEventListener("DOMContentLoaded", function(){
-
-    var pauseButton = document.getElementById("pause");
-    var resumeButton = document.getElementById("resume");
-
-    pauseButton.disabled = true;
-    resumeButton.disabled = true;
-    
+    // I feel like this is redundant since a global startButton is declared and the  
+    //  onclick fxn below should recognize this button....maybe necessary because after #main div is cleared, the 
+    // new startButton variable is only visible in this fxn--not so for "right", "wrong" etc  
+    // is the onclick fxn the issue?
+    var startButton=document.getElementById("startButton");
     startButton.onclick = function(){
-      // if (document.getElementById("game-alert")){
-      //   document.getElementById("game-alert").style.display = 'none';
-      // }
-      // stop();
-      pauseButton.disabled = false;
       questionCount = 0;
       right = 0;
       wrong = 0;
       missed = 0;
-      rightDiv.innerHTML = "<p>Correct: " + right;
-      wrongDiv.innerHTML = "<p>Wrong: " + wrong;
-      missedDiv.innerHTML = "<p>missed: " + missed + "</p>";
-      // var index1 = selectBox.selectedIndex;
-      // var index2 = difficultyBox.selectedIndex;
+
       callTrivia();
+      startButton.disabled=false;
+    }
+  
+
+  }
+
+  // ----------------Clicking Start Button will start the game-------------------------------
+
+    //Setting up startButton var and onclick function
+    // var startButton=document.getElementById("startButton");
+
+    startButton.onclick = function(){
+      questionCount = 0;
+      right = 0;
+      wrong = 0;
+      missed = 0;
+
+      callTrivia();
+      startButton.disabled=false;
     }
 
-    pauseButton.onclick = function(){
-     disableAnswerOnClick(ansOptions);
-      resumeButton.disabled = false;
-      stop();
-    }
-
-    resumeButton.onclick = function(){
-      answerOnClick(ansOptions);
-      resumeButton.disabled = true;
-      start();
-    }
-
-  });
+});
